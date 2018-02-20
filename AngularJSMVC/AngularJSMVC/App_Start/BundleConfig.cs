9e6db37fb57e5135f58a6b74a567dacbd896325f
LAPTOP-VOVLSUM9\Div <divyanand.tiwari@gmail.com>
2018-02-21 04:22:32 +0530

    Second commit.

---
+++ AngularJSMVC/AngularJSMVC/App_Start/BundleConfig.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Optimization;
+
+namespace AngularJSMVC
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/AwesomeAngularMVCApp")
+                    .IncludeDirectory("~/Scripts/Controllers", "*.js")
+                    .Include("~/Scripts/AwesomeAngularMVCApp.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/site.css"));
+
+            BundleTable.EnableOptimizations = true;
+        }
+    }
+}
